d81cc67e05a3464a1f820d196868b72eac90520e
El3veN <ghyxeleven@outlook.fr>
2014-09-06 09:42:18 +0200

    Update grab.js

--- grab.js
+++ grab.js
@@ -1,1 +1,17 @@
-API.getHistory()
+// Getpoints
+
+API.on(API.CHAT_COMMAND, callback);
+
+        function callback(value) {
+                if (value=='/getpoints') {
+                        var Uname=prompt("Enter username");
+                        users=API.getUsers();
+                        var len=users.length
+                        for(var i=0;i<len;++i){
+                        if (users[i].username==Uname) {
+                                 API.sendChat(Uname+" : WOOT POINTS : "+users[i].listenerPoints+" | DJ POINTS : "+ users[i].djPoints+" | GRAB POINTS : "+ users[i].curatorPoints)
+                                                      }
+                                                   
+                                                }
+                                 }
+                         }
