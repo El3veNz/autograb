463468e5a80234c35603c9fc19c3fa769aa37d64
El3veN <ghyxeleven@outlook.fr>
2014-10-18 22:01:32 +0200

    Update grab.js

--- grab.js
+++ grab.js
@@ -1,17 +1,40 @@
-// Getpoints
+//                           //
+//   AutoGrab para Plug DJ   //
+//                           //
 
-API.on(API.CHAT_COMMAND, callback);
+Grab={};
 
-        function callback(value) {
-                if (value=='/getpoints') {
-                        var Uname=prompt("Enter username");
-                        users=API.getUsers();
-                        var len=users.length
-                        for(var i=0;i<len;++i){
-                        if (users[i].username==Uname) {
-                                 API.sendChat(Uname+" : WOOT POINTS : "+users[i].listenerPoints+" | DJ POINTS : "+ users[i].djPoints+" | GRAB POINTS : "+ users[i].curatorPoints)
-                                                      }
-                                                   
-                                                }
-                                 }
-                         }
+Grab.check = function(e)
+{
+  if(e===null)
+  {
+    API.chatLog("Autograb: There's no DJ",true);
+    Grab.djname = null;
+  }
+  else if(API.getDJ().username!=API.getUser().username)
+  {
+    Grab.grab();
+    Grab.djname=API.getDJ().username;
+  }
+  else
+  {
+    if(Grab.djname!=API.getUser().username)
+    {
+      API.chatLog("Autograb: You are now DJ",false);
+      Grab.djname=API.getDJ().username;
+    }
+  }
+};
+Grab.welcome = function()
+{
+  API.chatLog("Autograb by @Boss4L enabled!",true);
+  API.chatLog("Don't forget about autowoot!", true);
+};
+
+Grab.grab = function()
+{
+  $(".icon-curate").click();
+  $($(".curate").children(".menu").children().children()[0]).mousedown();};
+  Grab.welcome();
+  Grab.check();
+  API.on(API.DJ_ADVANCE,Grab.check);
